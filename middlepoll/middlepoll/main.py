453fdb76ad20a7a14fb0c990df4831d8eec6bbec
Niels Warncke <niels.warncke@gmail.com>
2022-05-24 03:35:33 +0200

    middleware sends to a queue and pollinator reads from it on localstack, cdk does not yet set the middleware QUEUE_NAME env

--- middlepoll/middlepoll/main.py
+++ middlepoll/middlepoll/main.py
@@ -1,5 +1,6 @@
 import os
 from typing import Union
+import json
 
 import click
 from dotenv import load_dotenv
@@ -7,6 +8,8 @@ from fastapi import Depends, FastAPI, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer
 from jose import JWTError, jwt
 from pydantic import BaseModel
+import boto3
+from botocore.config import Config
 
 load_dotenv()
 
@@ -20,6 +23,31 @@ ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
 
 
+AWS_REGION =  os.environ.get('AWS_REGION', "us-east-1")
+boto3_config = Config(
+    region_name = AWS_REGION,
+)
+
+sqs = None
+queue_url = None
+
+
+def send_message(node_id, pollen):
+    while True:
+        response = sqs.receive_message(
+            QueueUrl=queue_url,
+            AttributeNames=[
+                'SentTimestamp'
+            ],
+            MaxNumberOfMessages=1,
+            MessageAttributeNames=[
+                'All'
+            ],
+            VisibilityTimeout=0,
+            WaitTimeSeconds=1000
+        )
+
+
 class Token(BaseModel):
     access_token: str
     token_type: str
@@ -27,6 +55,11 @@ class Token(BaseModel):
 
 class TokenData(BaseModel):
     username: Union[str, None] = None
+
+
+class Pollen(BaseModel):
+    pollen_id: str
+    notebook: str
 
 
 def get_sample_token(username):
@@ -62,18 +95,32 @@ async def read_users_me(current_user: TokenData = Depends(get_current_user)):
     return current_user
 
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+@app.post("/pollen/", response_model=Pollen)
+def send_message(pollen: Pollen):
+    print(f"received message: {pollen}")
+    sqs.send_message(
+        QueueUrl=queue_url,
+        MessageBody=json.dumps(pollen.dict())
+    )
+    return pollen
 
 
 @click.command()
-@click.option("--port", type=int, default=8000)
+@click.option("--port", type=int, default=5555)
 @click.option("--host", default="0.0.0.0")
-def main(port: int, host: str):
+@click.option("--aws_endpoint", type=str, default=None, help="For localstack: http://localhost:4566 | For AWS: None")
+@click.option("--aws_profile", type=str, default=None, help="For localstack: localstack | For AWS: aws_profile")
+def main(port: int, host: str, aws_endpoint=None, aws_profile=None):
     """
     Run the server.
     """
+    global sqs, queue_url
+    sqs = boto3.client('sqs', config=boto3_config, region_name=AWS_REGION,
+                         endpoint_url=aws_endpoint)
+    queue_url = sqs.get_queue_url(
+        QueueName=os.environ["QUEUE_NAME"]
+    )['QueueUrl']
+
     import uvicorn
 
     print("sample token", get_sample_token("test"))
